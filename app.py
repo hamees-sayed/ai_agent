e9977f506a4b3f80b9d7722f5d7e4a0bbfe73621
Hamees Sayed <98336593+hamees-sayed@users.noreply.github.com>
2024-03-13 19:38:43 +0000

    v2

--- app.py
+++ app.py
@@ -5,20 +5,35 @@ from dotenv import load_dotenv
 import google.generativeai as genai
 from llama_index.llms.gemini import Gemini
 from llama_index.core.query_engine import PandasQueryEngine
+from pandasai import SmartDataframe
+from pandasai.llm.google_palm import GooglePalm
+from langchain_google_genai import ChatGoogleGenerativeAI
 from prompt import new_prompt, instruction_str
 
 load_dotenv()
 GOOGLE_API_KEY = os.getenv('GOOGLE_API_KEY')
 genai.configure(api_key=GOOGLE_API_KEY)
 
-def generate_response(file_path, query, instruction_str=instruction_str, new_prompt=new_prompt, temperature=0.5):
+def png_exists(directory):
+    for filename in os.listdir(directory):
+        if filename.endswith(".png"):
+            return True
+    return False
+
+def delete_png(directory):
+    for filename in os.listdir(directory):
+        if filename.endswith(".png"):
+            file_path = os.path.join(directory, filename)
+            os.remove(file_path)
+
+
+def generate_response(file_path, query, temperature=0.5):
     df = pd.read_csv(file_path)
-    llm = Gemini(model="gemini-pro", temperature=temperature)
+    llm = ChatGoogleGenerativeAI(model="gemini-pro", temperature=temperature)
 
-    query_engine = PandasQueryEngine(df=df, llm=llm, instruction_str=instruction_str)
-    query_engine.update_prompts({"pandas_prompt": new_prompt})
-
-    return query_engine.query(query)
+    pdi = SmartDataframe(df, config={"llm": llm})
+    
+    return pdi.chat(query)
 
 
 def main():
@@ -43,12 +58,15 @@ def main():
             
     if query:
         if csv is not None:
+            delete_png("./exports/charts")
             st.chat_message("user").write(query)
             st.session_state.messages.append({"role": "user", "content": query})
-            print(csv.name, query)
             response = generate_response(csv, query, temperature)
             with st.chat_message("assistant"):
-                st.markdown(response)
+                if png_exists("./exports/charts"):
+                    st.image(response)
+                else:
+                    st.markdown(response)
             st.session_state.messages.append({"role": "assistant", "content": response})
             
 
